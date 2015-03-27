3491e1de6ac44504040123f781399d6094656409
max1k <maxkvasnikov@gmail.com>
2015-03-27 17:03:27 +0300

    Some design changes

--- p311/views.py
+++ p311/views.py
@@ -12,7 +12,7 @@ from django.utils.decorators import method_decorator
 class P311ListView(ListView):
 	model = File
 	context_object_name = 'files'
-	paginate_by = 20
+	paginate_by = 15
 
 	def get_queryset(self):
 		qs=File.objects.all().order_by('-doc_date')
